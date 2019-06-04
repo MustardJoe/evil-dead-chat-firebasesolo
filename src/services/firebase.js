50c0ebb4acbef5ca3cebe28a23a6dc838973009d
Jonathan Forney <jonathan.forney@gmail.com>
2019-06-03 17:03:35 -0700

    created firebase credentials

---
+++ src/services/firebase.js
@@ -0,0 +1,13 @@
+var config = {
+    apiKey: 'AIzaSyBGRZ5ISUNW_cpy-HGzVgn23hXYmYZHKao',
+    authDomain: 'jon-project-chat.firebaseapp.com',
+    databaseURL: 'https://jon-project-chat.firebaseio.com',
+    projectId: 'jon-project-chat',
+    storageBucket: 'jon-project-chat.appspot.com',
+    messagingSenderId: '416109174858',
+    appId: '1:416109174858:web:4a495ca8d81fb507'
+};
+
+// Initialize Firebase
+firebase.initializeApp(config);
+export const auth = firebase.auth();
