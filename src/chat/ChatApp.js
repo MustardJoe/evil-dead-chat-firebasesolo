84201c562dbcc32beaacea2f2790e1add9e42c1a
Jonathan Forney <jonathan.forney@gmail.com>
2019-06-06 21:08:45 -0700

    +began filling out ChatApp component

--- src/chat/ChatApp.js
+++ src/chat/ChatApp.js
@@ -1,9 +1,46 @@
 import Component from '../Component.js';
+import { auth, chatRoomsRef } from '../services/firebase.js';
 
 class ChatApp extends Component {
+    render() { //render method from addChatRoom, will need to modify
+        const dom = this.renderDOM();
+        const input = dom.querySelector('input');
+        const form = dom.querySelector('form');
+
+        dom.addEventListener('submit', event => {
+            event.preventDefault();
+            // this generates a random key and assigns to returned ref
+            const chatRoomRef = chatRoomsRef.push();
+
+            chatRoomRef.set({
+                key: chatRoomRef.key,
+                owner: auth.currentUser.uid,
+                name: input.value 
+            }).then(() => {
+                form.reset();
+            });
+           
+        });
+
+
+
+
+        return form;
+    }
+
     renderTemplate() {
         return /*html*/`
-            <div class="chat-room"
+            <div class="chat-room">
+                <div class="chat-content">
+                    
+                </div>
+                <div class="chat-input">
+                    <form>
+                        <input class="text-input" type="text">
+                        <button>Send</button>
+                    </form>
+                </div>
+            </div>
     `;
     }
 }
