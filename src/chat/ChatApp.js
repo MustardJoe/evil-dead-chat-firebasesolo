6edd682b30f363fd8b8e1018493514cc65b02990
Jonathan Forney <jonathan.forney@gmail.com>
2019-06-07 16:48:19 -0700

    refactored slash corrected ChatApp

--- src/chat/ChatApp.js
+++ src/chat/ChatApp.js
@@ -1,6 +1,7 @@
 import Component from '../Component.js';
 import Header from '../shared/Header.js';
-import { auth, chatRoomsRef, testtdata } from '../services/firebase.js';
+import QUERY from '../QUERY.js';
+import { auth, chatRoomsRef, chatMsgsByRoomRef } from '../services/firebase.js';
 
 class ChatApp extends Component {
     render() { //render method from addChatRoom, will need to modify
@@ -11,30 +12,52 @@ class ChatApp extends Component {
         const header = new Header();
         dom.prepend(header.render());
 
-        dom.addEventListener('submit', event => {
-            event.preventDefault();
-            // this generates a random key and assigns to returned ref
-            const chatRoomRef = chatRoomsRef.push();
-            console.log(chatRoomRef);
+        const searchParams = QUERY.parse(window.location.search.slice(1));
+        const chatRoomRef = chatRoomsRef.child(searchParams.key);
+        const roomMessagesRef = chatMsgsByRoomRef.child(searchParams.key);
+        
 
-            testtdata.on('value', snapshot => {
-                // console.log(snapshot.val()),
-                const testValue = snapshot.val();
-                console.log(testValue);
-                return testValue;
-            });
+        // dom.addEventListener('submit', event => {  //probably don't need any of this crap
+        //     event.preventDefault();
+        //     // this generates a random key and assigns to returned ref
+        //     const chatRoomRef = chatRoomsRef.push();
+        //     console.log(chatRoomRef);
 
-            chatRoomRef.set({
-                key: chatRoomRef.key,
-                owner: auth.currentUser.uid,
-                chat_msg: input.value, 
-                // name: auth.currentUser.username
-            }).then(() => {
-                form.reset();
+        //     testtdata.on('value', snapshot => {
+        //         // console.log(snapshot.val()),
+        //         const testValue = snapshot.val();
+        //         console.log(testValue);
+        //         return testValue;
+        //     });
+        // });
+
+        roomMessagesRef 
+            .on('value', snapshot => {
+                const value = snapshot.val();
+                const messages = value ? Object.values(value) : [];
+                messageList.update({ messages: messages });
             });
-           
+            
+        chatRoomRef
+            .on('value', snapshot => {
+                const value = snapshot.val();
+                header.update({ title: value.name });
+
+            });
+        
+        const messageInput = new MessageInput({ 
+            roomMessagesRef,
+            key: searchParams.key      
         });
-
+        
+        chatRoomRef.set({
+            key: chatRoomRef.key,
+            owner: auth.currentUser.uid,
+            chat_msg: input.value, 
+            // name: auth.currentUser.username
+        }).then(() => {
+            form.reset();
+        });
 
 
 
