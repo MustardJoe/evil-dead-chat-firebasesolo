0c059a98de15a802e2586660f1cbd00138f6507d
Jonathan Forney <jonathan.forney@gmail.com>
2019-06-08 13:16:35 -0700

    created msgList comp

---
+++ src/chat/MessageList.js
@@ -0,0 +1,35 @@
+import Component from '../Component.js';
+import MessageItem from './MessageItem.js';;
+
+class MessageList extends Component {
+    render() {
+        const dom = this.renderDOM();  
+
+        const messages = this.props.messages;
+
+        if(!messages) {
+            return dom;
+        }
+
+        messages.forEach(message => {
+            const messageItem = new MessageItem({ message });
+            dom.appendChild(messageItem.render());
+        });
+
+        setTimeout(() => {
+            dom.scrollTop = dom.scrollHeight;
+        });
+
+
+        return dom;
+    }
+
+    renderTemplate() {
+        return /*html*/`
+            <ul class="message-list"></ul>
+        `;
+
+    }
+}
+
+export default MessageList;
