f0c77f54e1fd064bb8a0ad8a3ffce94991d10c11
Jonathan Forney <jonathan.forney@gmail.com>
2019-06-08 13:18:00 -0700

    updated app-call-app to pass-make use of User auth info

--- src/chat/chat-room.js
+++ src/chat/chat-room.js
@@ -1,6 +1,13 @@
+import '../utils/check-auth.js';
 import ChatApp from './ChatApp.js';
-
+import { auth } from '../services/firebase.js';
 
 const root = document.getElementById('app');
-const app = new ChatApp();
-root.appendChild(app.render());
+
+auth.onAuthStateChanged(() => {
+    const app = new ChatApp();
+    root.appendChild(app.render());
+});
+
+// const app = new ChatApp();
+// root.appendChild(app.render());
