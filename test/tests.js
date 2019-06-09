6e7c1e96fe75534cf0550ae02f45d15b93d0dead
Jonathan Forney <jonathan.forney@gmail.com>
2019-06-09 14:39:18 -0700

    fix for travis maybe

--- test/tests.js
+++ test/tests.js
@@ -5,5 +5,5 @@ import './html-to-DOM.test.js';
 import './hash-storage.test.js';
 import './QUERY.test.js';
 import './Profile.test.js';
-// import { app } from '../src/services/firebase.js';
-// import './ChatRoomItem.test.js';
+import '../src/services/firebase.js';
+import './ChatRoomItem.test.js';
